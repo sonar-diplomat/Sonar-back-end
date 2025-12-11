b82acbc7d206c890d9800c4fc06b30378bebfd0a
Ermol <00ermzahar@gmail.com>
2025-12-11 19:17:28 +0300

    Update DTOs and proto definitions to change CollectionId and TrackId types from string to int, and adjust related properties in RecommendationsController

--- Application/DTOs/Recommendations/RecentCollectionDTO.cs
+++ Application/DTOs/Recommendations/RecentCollectionDTO.cs
@@ -2,7 +2,7 @@ namespace Application.DTOs.Recommendations;
 
 public class RecentCollectionDTO
 {
-    public required string CollectionId { get; set; }
+    public int CollectionId { get; set; }
     public int CollectionType { get; set; }
     public DateTime LastPlayedAtUtc { get; set; }
 }
